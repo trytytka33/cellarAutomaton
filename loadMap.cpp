c10e756d2df59c8b99668d057485691971bc6316
Kamil <kamil.siarkowicz@op.pl>
2025-12-06 13:46:33 +0100

    Add loadMap

---
+++ loadMap.cpp
@@ -0,0 +1,12 @@
+#include"loadMap.h"
+
+LoadMap::LoadMap(int width, int height) : background(map) {
+	try {
+		map.loadFromFile("map.png", false, sf::IntRect({ 0, 0 }, { width, height }));
+	}
+	catch (sf::Exception e) {
+		e.what();
+	}
+
+	background.setTexture(map, true);
+}
---
+++ loadMap.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class LoadMap {
+private:
+	sf::Texture map;
+	sf::Sprite background;
+
+public:
+	LoadMap(int width, int height);
+
+	sf::Sprite getBackground() {
+		return background;
+	}
+};
